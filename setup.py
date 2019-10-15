4c756d939ddacf97d6471da96bf794309f988e57
Alex <alex@datarootss-mbp.rto.be>
2019-10-15 11:59:42 +0200

    first commit for small pyspark project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='example',
+      version='0.0.1',
+      description='A sample PySpark application',
+      author='Alexandre Carlier',
+      author_email='',
+      packages=['gill'],
+      zip_safe=False)
